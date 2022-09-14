d5083a50dd96877522e9a75d40d160c682099b31
Fadlan Amrullah <51012056+fadlanam30@users.noreply.github.com>
2022-09-14 21:05:32 +0700

    init commit

--- app/src/main/java/com/inditech/activitysample/MainActivity.kt
+++ app/src/main/java/com/inditech/activitysample/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvResult: TextView
     private lateinit var tvResult2: TextView
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
