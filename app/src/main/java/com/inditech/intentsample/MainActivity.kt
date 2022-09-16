12e3da01b369bd6ddeb8bc16ba7c25b8f93e57d8
Naufal Ulwan <75620422+opangpoo123@users.noreply.github.com>
2022-09-16 22:05:14 +0700

    Parcelable

--- app/src/main/java/com/inditech/intentsample/MainActivity.kt
+++ app/src/main/java/com/inditech/intentsample/MainActivity.kt
@@ -9,34 +9,33 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var edEmail: EditText
+    private lateinit var edtEmail: EditText
     private lateinit var edtPassword: EditText
     private lateinit var btnLogin: Button
-    private lateinit var tvResult: TextView
-    private lateinit var tvResult2: TextView
     private lateinit var intentActivity: Intent
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        edEmail = findViewById(R.id.edEmail)
+        edtEmail = findViewById(R.id.edEmail)
         edtPassword = findViewById(R.id.edPassword)
         btnLogin = findViewById(R.id.btnLogin)
-        tvResult = findViewById(R.id.tvResult)
-        tvResult2 = findViewById(R.id.tvResult2)
+
         intentActivity = Intent(this@MainActivity, HomePage::class.java)
 
         btnLogin.setOnClickListener {
-            tvResult.visibility = View.VISIBLE
-            tvResult.text = edEmail.text
-            tvResult2.visibility = View.VISIBLE
-            tvResult2.text = edtPassword.text
 
-            intentActivity.putExtra(HomePage.EXTRA_EMAIL, edEmail.text.toString())
-            intentActivity.putExtra(HomePage.EXTRA_PASSWORD, edtPassword.text.toString())
+            val loginObject = Login(
+                edtEmail.text.toString(),
+                edtPassword.text.toString(),
+                true
+            )
 
+            intentActivity.putExtra(HomePage.EXTRA_LOGIN, loginObject)
             startActivity(intentActivity)
+
         }
 
     }
